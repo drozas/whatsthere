57df55220b1a8cb6e3e9a5c3979841746da00c7a
Simona Puica <Simona Puica@I10840>
2019-03-12 08:01:48 +0100

    arreglos en las actividades

--- WhatsThere/app/src/main/java/es/whatsthere/ImageActivity.java
+++ WhatsThere/app/src/main/java/es/whatsthere/ImageActivity.java
@@ -1,17 +1,23 @@
 package es.whatsthere;
 
 import android.content.Intent;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class ImageActivity extends AppCompatActivity implements View.OnClickListener{
+public class ImageActivity extends AppCompatActivity implements View.OnClickListener {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_IMAGE_GALLERY = 2;
-    Button cameraButton, galleryButton, backButton;
+    Button cameraButton, galleryButton, descriptionButton;
+    private ImageView selectedImage;
+    private Bitmap currentImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +26,18 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
 
         cameraButton = findViewById(R.id.cameraButton);
         galleryButton = findViewById(R.id.galleryButton);
+        descriptionButton = findViewById(R.id.descriptionButton);
+        selectedImage = findViewById(R.id.selectedImage);
 
         cameraButton.setOnClickListener(this);
         galleryButton.setOnClickListener(this);
+        descriptionButton.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.cameraButton:
 
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -40,12 +49,60 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
 
             case R.id.galleryButton:
 
-/**                Intent selectFromGalleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                Intent selectFromGalleryIntent = new Intent(Intent.ACTION_PICK);
+                selectFromGalleryIntent.setType("image/*");
                 if (selectFromGalleryIntent.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(selectFromGalleryIntent, REQUEST_IMAGE_GALLERY);
                 }
-**/
+
                 break;
+
+            case R.id.descriptionButton:
+                if (selectedImage != null) {
+                    // TODO
+                } else {
+                    // TODO
+                }
+
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri imageUri;
+
+        if (requestCode == RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_IMAGE_CAPTURE:
+                    imageUri = data.getData();
+                    if(imageUri != null){
+                        try{
+                            currentImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                            selectedImage.setImageBitmap(currentImage);
+                        }
+                        catch (Exception e){
+                            e.printStackTrace(); // TODO manejar el posible error
+                        }
+                    }
+
+                    break;
+                case REQUEST_IMAGE_GALLERY:
+                    imageUri = data.getData();
+                    if(imageUri != null){
+                        try{
+                            currentImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                            selectedImage.setImageBitmap(currentImage);
+                        }
+                        catch (Exception e){
+                            e.printStackTrace(); // TODO manejar el posible error
+                        }
+                    }
+
+                    break;
+            }
+
         }
     }
 }
