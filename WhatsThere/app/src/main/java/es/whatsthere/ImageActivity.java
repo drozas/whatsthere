efc7da41465f4a66bdbf2accfb150afe0c2d5eae
Simona Puica <Simona Puica@I10840>
2019-03-20 17:21:53 +0100

    cambios camara

--- WhatsThere/app/src/main/java/es/whatsthere/ImageActivity.java
+++ WhatsThere/app/src/main/java/es/whatsthere/ImageActivity.java
@@ -2,14 +2,22 @@ package es.whatsthere;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ImageActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -17,7 +25,7 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
     static final int REQUEST_IMAGE_GALLERY = 2;
     Button cameraButton, galleryButton, descriptionButton;
     private ImageView selectedImage;
-    private Bitmap currentImage;
+    private String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +48,7 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()) {
             case R.id.cameraButton:
 
-                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-                }
+                dispatchTakePictureIntent();
 
                 break;
 
@@ -79,8 +84,11 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
                     imageUri = data.getData();
                     if(imageUri != null){
                         try{
-                            currentImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
-                            selectedImage.setImageBitmap(currentImage);
+//                            Bundle extras = data.getExtras();
+//                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//                            selectedImage.setImageBitmap(imageBitmap);
+
+                            setPic();
                         }
                         catch (Exception e){
                             e.printStackTrace(); // TODO manejar el posible error
@@ -92,7 +100,7 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
                     imageUri = data.getData();
                     if(imageUri != null){
                         try{
-                            currentImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                          Bitmap  currentImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
                             selectedImage.setImageBitmap(currentImage);
                         }
                         catch (Exception e){
@@ -105,4 +113,63 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
 
         }
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,"es.whatsthere.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = selectedImage.getWidth();
+        int targetH = selectedImage.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        selectedImage.setImageBitmap(bitmap);
+    }
+
 }
